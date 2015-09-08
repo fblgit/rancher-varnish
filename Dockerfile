69e98a6d4d8a8fa3af3f1791b21b480ba595ae61
Manel Martinez Gonzalez <manel@nixelsolutions.com>
2015-09-09 00:24:58 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM ubuntu:14.04
+
+MAINTAINER Manel Martinez <manel@nixelsolutions.com>
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && \
+    apt-get -y install curl apt-transport-https supervisor
+
+RUN curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
+RUN echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.0" >> /etc/apt/sources.list.d/varnish-cache.list
+
+RUN apt-get update && \
+    apt-get -y install varnish
+
+ENV VARNISH_CFG_DIR /etc/varnish
+ENV BACKEND_SERVERS **ChangeMe**
+ENV BACKEND_PORT 80
+ENV BACKEND_HEALTHCHECK /
+ENV MAX_CACHE_SIZE 256M
+ENV HTTP_PORT 80
+
+ENV DEBUG 0
+
+VOLUME ${VARNISH_CFG_DIR}
+
+EXPOSE ${HTTP_PORT}
+
+RUN mkdir -p /var/log/supervisor
+RUN mkdir -p /usr/local/bin
+ADD ./bin /usr/local/bin
+RUN chmod +x /usr/local/bin/*.sh
+ADD ./etc/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+ADD .${VARNISH_CFG_DIR} ${VARNISH_CFG_DIR}
+
+CMD ["/usr/local/bin/run.sh"]
---
+++ bin/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+[ "$DEBUG" == "1" ] && set -x && set +e
+
+if [ "${BACKEND_SERVERS}" == "**ChangeMe**" -o -z "${BACKEND_SERVERS}" ]; then
+   echo "*** ERROR: you need to define BACKEND_SERVERS environment variable - Exiting ..."
+   exit 1
+fi
+
+if [ "${BACKEND_PORT}" == "**ChangeMe**" -o -z "${BACKEND_PORT}" ]; then
+   echo "*** ERROR: you need to define BACKEND_PORT environment variable - Exiting ..."
+   exit 1
+fi
+
+if [ "${BACKEND_HEALTHCHECK}" == "**ChangeMe**" -o -z "${BACKEND_HEALTHCHECK}" ]; then
+   echo "*** ERROR: you need to define BACKEND_HEALTHCHECK environment variable - Exiting ..."
+   exit 1
+fi
+
+if [ "${MAX_CACHE_SIZE}" == "**ChangeMe**" -o -z "${MAX_CACHE_SIZE}" ]; then
+   echo "*** ERROR: you need to define MAX_CACHE_SIZE environment variable - Exiting ..."
+   exit 1
+fi
+
+if [ "${HTTP_PORT}" == "**ChangeMe**" -o -z "${HTTP_PORT}" ]; then
+   echo "*** ERROR: you need to define HTTP_PORT environment variable - Exiting ..."
+   exit 1
+fi
+
+echo "=> Configuring Varnish..."
+cp ${VARNISH_CFG_DIR}/start.vcl ${VARNISH_CFG_DIR}/default.vcl
+backends=0
+default_director="sub vcl_init {\n  new bar = directors.round_robin();"
+for backend in `echo ${BACKEND_SERVERS} | sed "s/,/ /g"`; do
+   echo "=> Adding backend $backend to Varnish configuration..."
+   backend_id=$((backends++))
+   echo "\
+backend web${backend_id} { \
+  .host = \"${backend}\"; \
+  .port = \"${BACKEND_PORT}\"; \
+  .probe = { .url = \"${BACKEND_HEALTHCHECK}\"; .timeout = 5s; .interval = 5s; .window = 5; .threshold = 3; } \
+}" >> ${VARNISH_CFG_DIR}/default.vcl
+   default_director="${default_director}\n    bar.add_backend(web${backend_id});"
+done
+default_director="${default_director}\n}"
+echo -e "\n${default_director}" >> ${VARNISH_CFG_DIR}/default.vcl
+cat ${VARNISH_CFG_DIR}/end.vcl >> ${VARNISH_CFG_DIR}/default.vcl
+
+echo "=> Starting Varnish with these parameters:"
+echo "MAX_CACHE_SIZE = ${MAX_CACHE_SIZE}"
+echo "HTTP_PORT = ${HTTP_PORT}"
+echo "BACKEND_SERVERS = ${BACKEND_SERVERS}"
+
+/usr/bin/supervisord
